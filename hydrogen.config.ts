3880007614b6b4c9d29a5418ac0685ea882835ab
Pedro Henrique <pedrovasalmeida@gmail.com>
2022-06-23 21:23:00 -0300

    feat: :wrench: hydrogen file

--- hydrogen.config.ts
+++ hydrogen.config.ts
@@ -2,8 +2,8 @@ import {defineConfig} from '@shopify/hydrogen/config';
 
 export default defineConfig({
   shopify: {
-    storeDomain: 'hydrogen-preview.myshopify.com',
-    storefrontToken: '3b580e70970c4528da70c98e097c2fa0',
-    storefrontApiVersion: '2022-07',
+    storeDomain: 'hydrogen-dev-store.myshopify.com',
+    storefrontToken: '5a6ce371455a08294773ad7077ed246e',
+    storefrontApiVersion: '2022-04',
   },
 });
