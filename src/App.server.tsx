86c9b60f726e148aa616709bd703149caa0879f0
Pedro Henrique <pedrovasalmeida@gmail.com>
2022-06-23 21:48:42 -0300

    wip

--- src/App.server.tsx
+++ src/App.server.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import renderHydrogen from '@shopify/hydrogen/entry-server';
 import {Router, FileRoutes, ShopifyProvider} from '@shopify/hydrogen';
 import {Suspense} from 'react';
+import MiniCart from './components/MiniCart/index';
 
 function App() {
   return (
     <Suspense fallback={null}>
       <ShopifyProvider>
+        <MiniCart />
         <Router>
           <FileRoutes />
         </Router>
--- src/routes/index.server.tsx
+++ src/routes/index.server.tsx
@@ -1,5 +1,10 @@
-import React from 'react'
+import React from 'react';
 
 export default function Home() {
-  return <div className='font-black text-xl'>Hello my brudda</div>;
+  return (
+    <div className="font-black text-xl">
+      <h1>hello, brudda</h1>
+      <button>Carrinho</button>
+    </div>
+  );
 }
