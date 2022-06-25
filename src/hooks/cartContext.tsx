d9ee6e434430ff5689e3ec1c6b98b26f084ecb8c
Pedro Henrique <pedrovasalmeida@gmail.com>
2022-06-25 16:32:23 -0300

    chore: :recycle: move types to correct folder

--- src/hooks/cartContext.tsx
+++ src/hooks/cartContext.tsx
@@ -1,31 +1,6 @@
-import React, {
-  createContext,
-  useCallback,
-  useEffect,
-  useMemo,
-  useState,
-} from 'react';
+import React, {createContext, useMemo, useState} from 'react';
+import {CartContextProps, CartProps} from '../types/Cart';
 import {CartProduct} from '../types/Product';
-
-interface CartProps {
-  items: CartProduct[];
-  totalItemsCount: number;
-  uniqueItemsCount: number;
-  totalPrice: number;
-  shipping: number;
-  discounts: number;
-}
-
-interface CartContextProps {
-  cart: CartProps;
-  isCartOpen: boolean;
-  hooks: {
-    addItemToCart: (item: CartProduct) => void;
-    removeItemFromCart: (item: CartProduct) => void;
-    clearCart: () => void;
-    toggleCart: () => void;
-  };
-}
 
 const CartContext = createContext({} as CartContextProps);
 
