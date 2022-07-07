83ac96e18b9b727f4d3c1074b16a351644746f02
Pedro Henrique <pedrovasalmeida@gmail.com>
2022-07-06 23:42:18 -0300

    feat: :sparkles: create product page

---
+++ src/routes/products/[handle].server.tsx
@@ -0,0 +1,219 @@
+import React, {Suspense} from 'react';
+import {useRouteParams, useShopQuery, gql} from '@shopify/hydrogen';
+import {IProduct} from '../../types/Product';
+import ProductDetails from './productDetails.client';
+import Layout from '../../components/Layout.server';
+import BoxFallback from '../../components/BoxFallback/BoxFallback';
+
+interface QueryProps {
+  product: IProduct;
+}
+
+export default function Product() {
+  const {handle} = useRouteParams();
+  const {
+    data: {product},
+  } = useShopQuery<QueryProps>({
+    query: QUERY,
+    variables: {
+      handle,
+    },
+    preload: true,
+  });
+
+  return (
+    <Layout>
+      <Suspense fallback={<BoxFallback />}>
+        <ProductDetails product={product} />
+      </Suspense>
+    </Layout>
+  );
+}
+
+const QUERY = gql`
+  query product(
+    $country: CountryCode
+    $language: LanguageCode
+    $handle: String!
+  ) @inContext(country: $country, language: $language) {
+    product: product(handle: $handle) {
+      compareAtPriceRange {
+        maxVariantPrice {
+          currencyCode
+          amount
+        }
+        minVariantPrice {
+          currencyCode
+          amount
+        }
+      }
+      description
+      descriptionHtml
+      featuredImage {
+        url
+        width
+        height
+        altText
+      }
+      handle
+      id
+      media(first: 6) {
+        edges {
+          node {
+            ... on MediaImage {
+              mediaContentType
+              image {
+                id
+                url
+                altText
+                width
+                height
+              }
+            }
+            ... on Video {
+              mediaContentType
+              id
+              previewImage {
+                url
+              }
+              sources {
+                mimeType
+                url
+              }
+            }
+            ... on ExternalVideo {
+              mediaContentType
+              id
+              embedUrl
+              host
+            }
+            ... on Model3d {
+              mediaContentType
+              id
+              alt
+              mediaContentType
+              previewImage {
+                url
+              }
+              sources {
+                url
+              }
+            }
+          }
+        }
+      }
+      metafields(first: 20) {
+        edges {
+          node {
+            id
+            type
+            namespace
+            key
+            value
+            createdAt
+            updatedAt
+            description
+            reference {
+              __typename
+              ... on MediaImage {
+                id
+                mediaContentType
+                image {
+                  id
+                  url
+                  altText
+                  width
+                  height
+                }
+              }
+            }
+          }
+        }
+      }
+      priceRange {
+        maxVariantPrice {
+          currencyCode
+          amount
+        }
+        minVariantPrice {
+          currencyCode
+          amount
+        }
+      }
+      seo {
+        description
+        title
+      }
+      title
+      variants(first: 250) {
+        edges {
+          node {
+            availableForSale
+            compareAtPriceV2 {
+              amount
+              currencyCode
+            }
+            id
+            image {
+              id
+              url
+              altText
+              width
+              height
+            }
+            metafields(first: 10) {
+              edges {
+                node {
+                  id
+                  type
+                  namespace
+                  key
+                  value
+                  createdAt
+                  updatedAt
+                  description
+                  reference {
+                    __typename
+                    ... on MediaImage {
+                      id
+                      mediaContentType
+                      image {
+                        id
+                        url
+                        altText
+                        width
+                        height
+                      }
+                    }
+                  }
+                }
+              }
+            }
+            priceV2 {
+              amount
+              currencyCode
+            }
+            selectedOptions {
+              name
+              value
+            }
+            sku
+            title
+            unitPrice {
+              amount
+              currencyCode
+            }
+            unitPriceMeasurement {
+              measuredType
+              quantityUnit
+              quantityValue
+              referenceUnit
+              referenceValue
+            }
+          }
+        }
+      }
+      vendor
+    }
+  }
+`;
--- src/types/Product.ts
+++ src/types/Product.ts
@@ -5,3 +5,77 @@ export interface CartProduct {
   quantity: number;
   isAvailable: boolean;
 }
+
+interface ProductImage {
+  id: string;
+  url: string;
+  altText: string | null;
+  width: number;
+  height: number;
+}
+export interface IProduct {
+  description: string;
+  compareAtPriceRange: {
+    maxVariantPrice: {
+      currencyCode: string;
+      amount: string;
+    };
+    minVariantPrice: {
+      currencyCode: string;
+      amount: string;
+    };
+  };
+  featuredImage: {
+    url: string;
+    width: number;
+    height: number;
+    altText: string | null;
+  };
+  handle: string;
+  id: string;
+  media: {
+    edges: {
+      node: {
+        mediaContentType: string;
+        image: ProductImage;
+      };
+    }[];
+  };
+  priceRange: {
+    maxVariantPrice: {
+      currencyCode: string;
+      amount: string;
+    };
+    minVariantPrice: {
+      currencyCode: string;
+      amount: string;
+    };
+  };
+  seo: {
+    description: string | null;
+    title: string | null;
+  };
+  title: string;
+  variants: {
+    edges: {
+      node: {
+        availableForSale: boolean;
+        compareAtPriceV2: any;
+        id: string;
+        image: ProductImage;
+        priceV2: {
+          amount: string;
+          currencyCode: string;
+        };
+        selectedOptions: {
+          name: string;
+          value: string;
+        }[];
+        sku: string;
+        title: string;
+        unitPrice: any;
+      };
+    }[];
+  };
+  vendor: string;
+}
